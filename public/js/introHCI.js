49d6a83dd48f8318517b1856f1f5469fa7b6e016
Kai-Chin Shih <k3shih@ucsd.edu>
2018-02-22 10:57:53 -0800

    unfinished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(function(){
+}
+
+function clickLikeBtn() {
+	e.preventDefault();
+	ga('create', 'UA-114642382-1', 'auto');
+	ga('send', 'event', 'like', 'click');
 }
